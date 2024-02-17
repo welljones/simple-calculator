b97a61922aeda8e64b05bd73cbb3d6543616f382
Welljones <wjmhate@gmail.com>
2024-02-17 22:35:50 +0800

    Finalizing Desing & function v1

--- index.js
+++ index.js
@@ -11,5 +11,10 @@ function clearDisplay(){
 }
 
 function calculate(){
-
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Syntax Error";
+    }
 }
